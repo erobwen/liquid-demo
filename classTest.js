a3eabba3976be108d00148c255f0c1fd45591363
Robert Renbris <robert.wensman@gmail.com>
2017-11-27 20:14:58 +0100

    ...

--- classTest.js
+++ classTest.js
@@ -1,33 +1,67 @@
 
 
-	class Foobar {
-		constructor() {
-			this.member = 10;
-		}
+class Foobar {
+	constructor() {
+		this.member = 10;
+	}
+	
+	somePrototypeFunctionInClass() {
+		this.member = 1000;
+	}
+}
+Foobar.prototype.prototypeMember = "fum";
+let FoobarPrototype = Foobar.prototype;
+Foobar.prototype.prototypeFunction = function() {
+	return true;
+}
+
+let x = new Foobar();
+
+x.decoration = "bar";
+
+// Key iteration: (non inherited properties only...)
+console.log("key iteration");
+console.log(Object.keys(x));
+
+// For iteration... 
+console.log("for iteration"); 
+for(property in x) {
+	console.log(property);
+}
+
+
+
+
+	
+// let i;
+// let dummy;
+
+	// console.time("instanceof");
+	// i = 1;
+	// while (i++ < 10000000) {
+		// dummy = x instanceof Foobar;
+	// }
+	// console.timeEnd("instanceof");
 		
-		somePrototypeFunctionInClass() {
-			this.member = 1000;
-		}
-	}
-	Foobar.prototype.prototypeMember = "fum";
-	let FoobarPrototype = Foobar.prototype;
-	Foobar.prototype.prototypeFunction = function() {
-		return true;
-	}
+	// console.time("member check");
+	// i = 1;
+	// while (i++ < 10000000) {
+		// dummy = typeof(x.decoration) !== 'undefined';
+	// }
+	// console.timeEnd("member check");
 
-	let x = new Foobar();
+	// console.time("member check inherited");
+	// i = 1;
+	// while (i++ < 10000000) {
+		// dummy = typeof(x.prototypeFunction) !== 'undefined';
+	// }
+	// console.timeEnd("member check inherited");
 
-	x.decoration = "bar";
-
-	console.log(Object.keys(x));
-
-	for(property in x) {
-		console.log(property);
-	}
-
-
-
-
+	
+	
+	
+	
+	
 // Foobar.prototype.isPrototype = true;
 
 // // console.log(Foobar);
